c988bcc27ac030137a14ea3424d91be8ab604c2a
promise <976949689@qq.com>
2024-10-26 16:55:07 +0800

    2024年10月26日16:55:05

--- SLF4J_STUDY/src/test/java/com/clearlove/slf4j/test01/SLF4JTest01.java
+++ SLF4J_STUDY/src/test/java/com/clearlove/slf4j/test01/SLF4JTest01.java
@@ -84,4 +84,42 @@ public class SLF4JTest01 {
     }
   }
 
+  @Test
+  public void test04() {
+    /*
+      SLF4]日志门面，共有3种情况对日志实现进行绑定
+      1.在没有绑定任何日志实现的基础之上，日志是不能够绑定实现任何功能的
+        值得大家注意的是，通过我们刚刚的演示，slf4j-simple是slf4j官方提供的
+        使用的时候，也是需要导入依赖，自动绑定到slf4j门面上
+        如果不导入，slf4j核心依赖是不提供任何实现的
+      2.logback和simple(包括nop)
+        都是slf4j门面时间线后面提供的日志实现，所以API完全遵循slf4j进行的设计
+        那么我们只需要导入想要使用的日志实现依赖，即可与slf4j无缝衔接
+        值得一提的是nop虽然也划分到实现中了，但是他是指不实现日志记录（后续课程）
+      3.log4j和JUL
+        都是slf4j门面时间线前面的日志实现，所以API不遵循slf4j进行设计
+        通过适配桥接的技术，完成与日志门面的衔接
+
+      试着将logback日志框架集成进来
+      测试1：
+        在原有slf4j-simple日志实现的基础上，又集成了logback
+        通过测试，日志是打印出来了java.lang.ClassNotFoundException:aaa
+        通过这一句可以发现SLF4:Actual binding is of type[org.slf4j.impl.simpleLoggerFactory]
+        虽然集成了logback,但是我们现在使用的仍然是slf4j-simple
+        事实上只要出现了这个提示
+        SLF4J:Class path contains multiple SLF4J bindings.
+        在slf4j环境下，证明同时出现了多个日志实现
+        如果先导入logback依赖，后导入slf4j-simple依赖
+        那么默认使用的就是logback依赖
+        如果有多个日志实现的话，默认使用先导入的实现
+     */
+
+    Logger logger = LoggerFactory.getLogger(SLF4JTest01.class);
+    try {
+      Class.forName("aaa");
+    } catch (Exception e) {
+      logger.info("具体错误：", e);
+    }
+  }
+
 }
